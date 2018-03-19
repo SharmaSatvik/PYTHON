abdaab6d185d005dd9979b521dc426b8f0fdd9b4
SharmaSatvik <37412303+SharmaSatvik@users.noreply.github.com>
2018-03-19 15:41:08 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,8 @@
+def area_rectangle(l,b):
+    return l*b
+def main():
+    l=input('What is the length of the rectangle? \n')
+    b=input('What is the breadth of the rectangle? \n')
+    a=l*b
+    print "Area is the: "+ str(a)
+main()
